47986cc5c8da01e353617fa06b6af31519444d63
Val Vanderschaegen <valere.vanderschaegen@gmail.com>
2015-09-13 14:22:31 -0700

    Updating test to build all subtargets, removing some binaries.

---
+++ test/build.rs
@@ -0,0 +1,28 @@
+use std::process::Command;
+use std::env;
+
+fn main() {
+    cargo_dep("CSharp");
+    cargo_dep("cpp/dynamic");
+}
+
+fn cargo_dep(path: &str) {
+    let config = match env::var("PROFILE").unwrap_or_else(|e| panic!("Count not get confing from env {}", e)).as_ref() {
+        "debug" => "debug",
+        "release" => "release",
+        cfg => panic!("Unknown config {}", cfg)
+    };
+
+    //Run cargo
+    let mut cargo = Command::new("cargo");
+
+    cargo.current_dir(path)
+        .arg("build");
+
+    if config == "release" {
+        cargo.arg("--release");
+    }
+
+    cargo.output()
+        .unwrap_or_else(|e| panic!("Unable to run cargo {}", e));
+}
