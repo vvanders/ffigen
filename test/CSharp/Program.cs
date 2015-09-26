60a8086b87f70203348a27c7d1b9c17a7b4c35c1
Val Vanderschaegen <valere.vanderschaegen@gmail.com>
2015-09-26 12:54:19 -0700

    Adding NUnit, tests for CSharp

--- test/CSharp/Program.cs
+++ test/CSharp/Program.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using rust;
+﻿using rust;
 using System.Runtime.InteropServices;
+using NUnit.Framework;
 
 namespace CSharp
 {
--- test/CSharp/src/main.rs
+++ test/CSharp/src/main.rs
@@ -1,6 +1,15 @@
 ﻿use std::process::Command;
 
 fn main() {
+	run();
+}
+
+#[test]
+fn test() {
+	run();
+}
+
+fn run() {
 	let exec = Command::new(format!("bin/x64/{}/CSharp.exe", get_config()))
 		.output()
 		.unwrap_or_else(|e| panic!("Unable to run {}", e));
